6ef1a31207b9c3570bd4842f13c717758f934af5
alikhanrakhmet <alikhanrakhmet005@gmail.com>
2024-12-14 19:21:32 +0500

    rk

---
+++ src/main/java/com/example/lab7_smtp/Lab7SmtpApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab7_smtp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab7SmtpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab7SmtpApplication.class, args);
+	}
+
+}
